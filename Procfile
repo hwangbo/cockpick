9227a8f9036b1c084bd59b55b93ded5f3cf62a9a
hwangbo <taekwonod714@naver.com>
2019-08-13 19:46:32 +0900

    before heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cockpick.wsgi --log-file -
--- cocktail/settings.py
+++ cocktail/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'knp^09^(3($k6#h(vt-o=u#xetmps)^8ffq#1*ar2xqvrl0&b8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'knp^09^(3($k6#h(vt-o=u#xetmps)^8ffq#1*ar2xqvrl0&b8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +130,8 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
